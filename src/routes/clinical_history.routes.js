14465b9c31c5d7020dc55aa1452b4579d1955113
eduar <eduarcaco@hotmail.com>
2025-12-09 12:21:14 -0600

    Obtain medical history by patient ID

--- src/routes/clinical_history.routes.js
+++ src/routes/clinical_history.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const clinical_history_controller = require ('../controllers/clinical_history.controller');
 
 router.post('/:id_patient', clinical_history_controller.create);
-router.put('/:id_clinical_hisotry', clinical_history_controller.update);
+router.put('/:id_clinical_history', clinical_history_controller.update);
+router.get('/:id_patient', clinical_history_controller.getById);
 
 module.exports = router;
