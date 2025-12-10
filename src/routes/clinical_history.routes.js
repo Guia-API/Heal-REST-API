ed1e294b87f6563dd460b604f6f925a88f12a890
eduar <eduarcaco@hotmail.com>
2025-12-09 19:04:23 -0600

    Clinical history validators

--- src/routes/clinical_history.routes.js
+++ src/routes/clinical_history.routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const clinical_history_controller = require ('../controllers/clinical_history.controller');
 
-router.post('/:id_patient', clinical_history_controller.create);
-router.put('/:id_clinical_history', clinical_history_controller.update);
-router.get('/:id_patient', clinical_history_controller.getById);
+const {
+    create_clinical_history_schema,
+    update_clinical_history_schema,
+    get_clinical_history_schema
+} = require('../validators/clinical_history.schemas.validator')
+
+router.post('/:id_patient', create_clinical_history_schema, clinical_history_controller.create);
+router.put('/:id_clinical_history', update_clinical_history_schema, clinical_history_controller.update);
+router.get('/:id_patient', get_clinical_history_schema, clinical_history_controller.getById);
 
 module.exports = router;
