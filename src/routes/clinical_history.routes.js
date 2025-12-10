eb1c05a6cc86a423ee5045c6c6fc4def395dba30
eduar <eduarcaco@hotmail.com>
2025-12-09 19:36:11 -0600

    Fixed bug when calling validation schema methods in clinical history

--- src/routes/clinical_history.routes.js
+++ src/routes/clinical_history.routes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const clinical_history_controller = require ('../controllers/clinical_history.controller');
 
 const {
-    create_clinical_history_schema,
-    update_clinical_history_schema,
-    get_clinical_history_schema
-} = require('../validators/clinical_history.schemas.validator')
+    validate_create_clinical_history,
+    validate_update_clinical_history,
+    validate_get_clinical_history
+} = require('../validators/clinical_history.schemas.validator');
 
-router.post('/:id_patient', create_clinical_history_schema, clinical_history_controller.create);
-router.put('/:id_clinical_history', update_clinical_history_schema, clinical_history_controller.update);
-router.get('/:id_patient', get_clinical_history_schema, clinical_history_controller.getById);
+router.post('/:id_patient', validate_create_clinical_history, clinical_history_controller.create);
+router.put('/:id_clinical_history', validate_update_clinical_history, clinical_history_controller.update);
+router.get('/:id_patient', validate_get_clinical_history, clinical_history_controller.getById);
 
 module.exports = router;
